61bfb07fbf2bf67a85ecfbdacb7b018d5d285c0d
Braden Anderson <banderson@instructure.com>
2015-06-02 17:01:41 -0600

    add timing test

--- test.js
+++ test.js
@@ -3,11 +3,27 @@
 const eratosthing = require('./index');
 const assert = require('assert');
 
-const primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37];
-let generator = eratosthing();
+(function testFirstPrimes() {
+  const primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37];
+  let generator = eratosthing();
 
-primes.forEach(function(prime) {
-  assert.equal(generator.next().value, prime);
-});
+  primes.forEach(function(prime) {
+    assert.equal(generator.next().value, prime, 'first primes correct');
+  });
+})();
+
+(function testLargePrime() {
+  const iterations = 10 * 1000;
+  let generator = eratosthing();
+  let start = Date.now();
+  let value;
+
+  for (let i = 0; i < iterations; i++) {
+    value = generator.next().value;
+  }
+
+  assert.equal(value, 104729, '10kth prime correct');
+  assert(Date.now() - start < 200, '10k primes < 200ms');
+})();
 
 console.log('success!');
