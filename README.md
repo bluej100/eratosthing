4396a55f468fde7d934e3122ace2957de174b6be
Braden Anderson <banderson@instructure.com>
2016-02-25 16:13:16 -0700

    unroll main loop further using O'Neill's wheel

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 [![Build Status](https://travis-ci.org/bluej100/eratosthing.svg?branch=master)](https://travis-ci.org/bluej100/eratosthing)
 
-This implements in JavaScript an online algorithm for finding successive primes in O(n log n) using a priority queue of composites, as proposed by [Melissa O'Neill](https://www.cs.hmc.edu/~oneill/index.html) in her 2008 [The Genuine Sieve of Eratosthenes](http://www.cs.hmc.edu/~oneill/papers/Sieve-JFP.pdf). Finding her paper and adding her optimization of starting each filter at n^2, rather than n\*2, reduced runtime by about 40%.
+This implements in JavaScript an online algorithm for finding successive primes in O(n log n) using a priority queue of composites, as proposed by [Melissa O'Neill](https://www.cs.hmc.edu/~oneill/index.html) in her 2008 [The Genuine Sieve of Eratosthenes](http://www.cs.hmc.edu/~oneill/papers/Sieve-JFP.pdf).
+
+Finding her paper allowed me to reduce runtime by 60% by starting each filter at at n^2, rather than n\*2, and by using her unrolled wheel of candidates.
 
 Installation
 ------------
--- index.js
+++ index.js
@@ -4,14 +4,26 @@ const PriorityQueue = require('js-priority-queue');
 
 function scanComposites(composites, value) {
   let composite = false;
-  while (composites.length && composites.peek().value === value) {
-    composite = true;
+  while (composites.length && composites.peek().value <= value) {
     let next = composites.dequeue();
+    if (next.value === value) {
+      composite = true;
+    }
+
     next.value += next.delta;
     composites.queue(next);
   }
   return composite;
 }
+
+const wheel2357 = [
+  10, 2, 4, 2, 4, 6, 2, 6,
+  4, 2, 4, 6, 6, 2, 6, 4,
+  2, 6, 4, 6, 8, 4, 2, 4,
+  2, 4, 8, 6, 4, 6, 2, 4,
+  6, 2, 6, 6, 4, 2, 4, 6,
+  2, 6, 4, 2, 4, 2, 10, 2,
+];
 
 module.exports = function*(max) {
   let factorCap = max ? Math.sqrt(max) : undefined;
@@ -19,8 +31,14 @@ module.exports = function*(max) {
   let composites = new PriorityQueue({comparator: comparator});
 
   yield 2;
-  let value;
-  for (value = 3; true; value += 2) {
+  yield 3;
+  yield 5;
+  yield 7;
+
+  let value, notch;
+  for (value = 1, notch = 0; true; notch = (notch + 1) % wheel2357.length) {
+    value += wheel2357[notch];
+
     if (max && value > max) { break; }
     if (scanComposites(composites, value)) { continue; }
     yield value;
